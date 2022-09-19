f8354cf5a6c775bc39ad3e11d57517945f930c97
denisos <deniskos@hotmail.com>
2022-09-18 20:53:02 -0700

    added solutions for 7,8,9

--- src/07-union.problem.ts
+++ src/07-union.problem.ts
@@ -5,9 +5,11 @@ import { z } from "zod";
 
 const Form = z.object({
   repoName: z.string(),
-  privacyLevel: z.string(),
+  privacyLevel: z.enum(['public', 'private']),
   //              ^ 🕵️‍♂️
 });
+
+type FormType = z.infer<typeof Form>
 
 export const validateFormInput = (values: unknown) => {
   const parsedData = Form.parse(values);
--- src/09-composing-objects.problem.ts
+++ src/09-composing-objects.problem.ts
@@ -6,21 +6,35 @@ import { Equal, Expect } from "./helpers/type-utils";
  * while also making sure the cases don't go red!
  */
 
-const User = z.object({
-  id: z.string().uuid(),
+// could define id and reuse
+const IdSchema = z.string().uuid();
+
+// could define a base to inherit from
+const IdBaseSchema = z.object({
+  id: IdSchema
+});
+
+// const User = z.object({
+//   id: IdSchema,
+//   name: z.string(),
+// });
+
+const User = IdBaseSchema.extend({
   name: z.string(),
 });
 
 const Post = z.object({
-  id: z.string().uuid(),
+  id: IdSchema,
   title: z.string(),
   body: z.string(),
 });
 
 const Comment = z.object({
-  id: z.string().uuid(),
+  id: IdSchema,
   text: z.string(),
 });
+
+type UserType = z.infer<typeof User>
 
 type cases = [
   Expect<Equal<z.infer<typeof Comment>, { id: string; text: string }>>,
